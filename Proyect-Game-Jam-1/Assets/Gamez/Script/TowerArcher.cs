9cfc57772e1bd56d65e26ec6eeb86420a26cfb83
Zhebhqzx <zhebhqzx@gmail.com>
2025-05-12 17:14:38 -0500

    Nuevas Funciones
    Torre de Veneno
    Torre de Arquero

---
+++ Proyect-Game-Jam-1/Assets/Gamez/Script/TowerArcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class TowerArcher : TowerIA
+{
+    public int boostedAttackMultiplier = 2; // El ataque potenciado hace el doble de daño
+    private int attackCounter = 0;
+
+    // Sobrescribimos el método Attack
+    public new void Attack()
+    {
+        if (currentTarget == null) return;
+
+        attackCounter++;
+
+        if (attackCounter == 5)
+        {
+            // Ataque potenciado
+            currentTarget.TakeDamage(damage * boostedAttackMultiplier);
+            attackCounter = 0; // Reinicia el contador
+        }
+        else
+        {
+            // Ataque normal
+            currentTarget.TakeDamage(damage);
+        }
+    }
+
+    // Sobrescribimos la corrutina para usar el nuevo Attack
+    IEnumerator corrutineAttack()
+    {
+        while (true)
+        {
+            Attack();
+            yield return new WaitForSeconds(timeShoot);
+        }
+    }
+
+    // Start es igual, pero iniciamos la corrutina local
+    void Start()
+    {
+        StartCoroutine(corrutineAttack());
+    }
+}
